46ec0136347b3eb85c0019b888bc6e5c5f4aaa8e
Yevhen Lytvynchuk <evg.litik@gmail.com>
2022-09-11 17:28:43 +0300

    fix esc

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,7 +7,6 @@ export const Modal = ({ img, onClosseModal }) => {
     const handleKeydown = e => {
       if (e.code === 'Escape') {
         onClosseModal();
-        console.log('key esc');
       }
     };
 
